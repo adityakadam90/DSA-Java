207e9a87cf369f448231adf3f1a25544636bf13d
aditya <adityakadam992129@gmail.com>
2026-08-14 20:03:59 +0530

    find once appear element in array

---
+++ Array/High/onceApp.java
@@ -0,0 +1,16 @@
+public class onceApp{
+    public static void main(String args[]){
+        int arr[] = {3,4,5,7,3,5};
+        for(int i=0;i<arr.length;i++){
+            int count = 0;
+            for(int j=0;j<arr.length;j++){
+                if(arr[i] == arr[j]){
+                    count++;
+                }
+            }
+            if(count == 1){
+                System.out.println(arr[i]);
+            }
+        }
+    }
+}
