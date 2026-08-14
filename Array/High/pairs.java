522d6d67603615a2481df2c34e27d8a9bff54730
aditya <adityakadam992129@gmail.com>
2026-08-14 08:02:43 +0530

    pairs sum hit target...!

--- Array/High/pairs.java
+++ Array/High/pairs.java
@@ -1,11 +1,19 @@
 public class pairs {
     public static void main(String args[]){
-        int arr[] = {2,3,4,5,6};
+        int arr[] = {2,7,4,5,2,6};
+        int targetSum = 4;
+        int p = 0;
         for(int i=0;i<arr.length-1;i++){
-            for(int j=i;j<arr.length;j++){
-                System.out.print("("+arr[i]+","+arr[j]+")");
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]+arr[j] == targetSum){
+                    System.out.println("this pairs hit target sum = "+"("+arr[i]+","+arr[j]+")");
+                    p++;
+                }
             }
-            System.out.println();
+            //System.out.println();
+        }
+        if(p == 0){
+            System.out.println("not any pair hit target sum.");
         }
     }
     
