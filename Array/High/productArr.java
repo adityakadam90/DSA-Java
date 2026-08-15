1abc9b8d803e69ae4da42c381001e23aaa7face6
aditya <adityakadam992129@gmail.com>
2026-08-15 21:42:31 +0530

    productArr.java

---
+++ Array/High/productArr.java
@@ -0,0 +1,14 @@
+public class productArr {
+    public static void main(String args[]){
+        int arr[] = {2,3,4,5,6};
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length;j++){
+                    if(i != j){
+                        System.out.println(arr[i]+" x "+arr[j]+" = "+(arr[i]*arr[j]));
+                    }
+            }
+            System.out.println();
+        }
+    }
+    
+}
