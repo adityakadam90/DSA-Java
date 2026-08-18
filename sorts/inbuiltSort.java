e6a9b41cd99c307c8af7deb914c188a7e4872ad3
aditya <adityakadam992129@gmail.com>
2026-08-18 17:14:29 +0530

    Inbuilt Sort in java

---
+++ sorts/inbuiltSort.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+import java.util.Collections;
+public class inbuiltSort {
+    public static void main(String args[]){
+        int arr[] = {3,2,4,1,5};
+        Arrays.sort(arr);
+         System.out.println(Arrays.toString(arr));
+        // Arrays.sort(arr,Collections.reverseOrder());
+    }
+}
