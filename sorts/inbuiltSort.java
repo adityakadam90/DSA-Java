8a8e71bbf06dea0966809db32058babb1f6960d0
aditya <adityakadam992129@gmail.com>
2026-08-18 17:15:54 +0530

    Inbuilt Sort in java with collection for reverse..

--- sorts/inbuiltSort.java
+++ sorts/inbuiltSort.java
@@ -2,9 +2,8 @@ import java.util.Arrays;
 import java.util.Collections;
 public class inbuiltSort {
     public static void main(String args[]){
-        int arr[] = {3,2,4,1,5};
-        Arrays.sort(arr);
-         System.out.println(Arrays.toString(arr));
-        // Arrays.sort(arr,Collections.reverseOrder());
+        Integer arr[] = {3,2,4,1,5};
+        Arrays.sort(arr, Collections.reverseOrder());
+        System.out.println(Arrays.toString(arr));
     }
 }
