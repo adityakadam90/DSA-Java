1c99b347d9193ee609d7ee2032fc96c97917066f
aditya <adityakadam992129@gmail.com>
2026-08-24 20:07:33 +0530

    find the subString of string

---
+++ string/subStr.java
@@ -0,0 +1,29 @@
+// public class subStr {
+//     public static void main(String args[]){
+//         String str = new String("aditya_kadam");
+//         for(int i=0;i<str.length()-1;i++){
+//             for(int j=i+1;j<str.length();j++){
+//                 // System.out.println(str.charAt(i)+""+str.charAt(j));
+//                 System.out.println(str.substring(i, j));
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+public class subStr{
+    public static void main(String args[]){
+        String str = new String("helloWorld");
+        int c = 0;
+        for(int i=0;i<str.length();i++){
+            String temp = "";
+            for(int j=i;j<str.length();j++){
+                temp += str.charAt(j);
+                c++;
+                System.out.println(temp);;
+            }
+            // System.out.println();
+        }
+        System.out.println(c);
+    }
+}
